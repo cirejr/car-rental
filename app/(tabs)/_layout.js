e7f589553ec67b137d7b75e1e6950f15be50941d
cirejr <juniorcireba@gmail.com>
2023-06-22 00:32:51 +0000

    feature: made progress on the custom bottom tab

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,10 +1,27 @@
 import { Tabs } from "expo-router";
+import { TouchableOpacity } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+const TabButton = (props) => {
+  return <TouchableOpacity></TouchableOpacity>;
+};
 export default function Layout() {
   return (
     <SafeAreaProvider>
-      <Tabs>
+      <Tabs
+        screenOptions={{
+          tabBarStyle: {
+            position: "absolute",
+            borderRadius: 25,
+            bottom: 15,
+            paddingVertical: 5,
+            marginHorizontal: 10,
+            backgroundColor: "#26272E",
+          },
+          tabBarShowLabel: false,
+          tabBarButton: (props) => <TabButton {...props} />,
+        }}
+      >
         <Tabs.Screen name="tab1" options={{ tabBarLabel: "Tab1" }} />
         <Tabs.Screen name="tab2" options={{ tabBarLabel: "Tab2" }} />
       </Tabs>
