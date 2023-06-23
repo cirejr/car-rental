059daa50d8498d87e21eee8e5c9ef200936f1eed
cirejr <juniorcireba@gmail.com>
2023-06-23 00:38:43 +0000

    feature : custom tab finally ok

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,29 +1,56 @@
-import { Tabs } from "expo-router";
+import { Tabs, useRouter } from "expo-router";
 import { TouchableOpacity } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
 
-const TabButton = (props) => {
-  return <TouchableOpacity></TouchableOpacity>;
+{
+  /*const routes = {
+	HOME : 'home',
+	BOOKMARKS: 'bookmarks',
+	CATEGORIES: 'categories',
+	PROFILE : 'profile'
+}*/
+}
+
+export const screens = {
+  Tab1: "tab1",
+  Tab2: "tab2",
 };
+
+export const route = {
+  Tab1: "tab1",
+  Tab2: "tab2",
+};
+
 export default function Layout() {
   return (
     <SafeAreaProvider>
       <Tabs
-        screenOptions={{
+        screenOptions={({ route }) => ({
           tabBarStyle: {
             position: "absolute",
             borderRadius: 25,
             bottom: 15,
             paddingVertical: 5,
             marginHorizontal: 10,
-            backgroundColor: "#26272E",
+            backgroundColor: "indigo",
           },
           tabBarShowLabel: false,
-          tabBarButton: (props) => <TabButton {...props} />,
-        }}
+          tabBarIcon: ({ focused }) => {
+            let iconName;
+
+            if (route.name === screens.Tab1) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === screens.Tab2) {
+              iconName = focused ? "bookmark" : "bookmark-outline";
+            }
+
+            return <Ionicons name={iconName} size={22} color="white" />;
+          },
+        })}
       >
-        <Tabs.Screen name="tab1" options={{ tabBarLabel: "Tab1" }} />
-        <Tabs.Screen name="tab2" options={{ tabBarLabel: "Tab2" }} />
+        <Tabs.Screen name={screens.Tab1} options={{ tabBarLabel: "Tab1" }} />
+        <Tabs.Screen name={screens.Tab2} options={{ tabBarLabel: "Tab2" }} />
       </Tabs>
     </SafeAreaProvider>
   );
