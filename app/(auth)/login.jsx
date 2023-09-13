35288f858f32b71be7f0be95196443d7341e7fdb
cirejr <juniorcireba@gmail.com>
2023-09-13 17:26:35 +0000

    feature: working on the login screen

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -31,14 +31,29 @@ const Login = () => {
 	}
 	
   return (
-	<View className="flex-1 justify-center items-center">
-		<Spinner visible={true} animation='slide' />
+	<View className="flex-1 justify-center items-center px-5 space-y-3">
+		<View className="absolute h-40 w-40 rounded-full left-[-20] top-20 bg-indigo-600 ">
+		</View>
+		<View className="absolute h-60 w-60 rounded-full right-[-10] top-5 bg-fuchsia-800">
+		</View>
+
+		<Spinner visible={false} animation='slide' />
 		<TextInput
+			clearButtonMode='always'
 			autoCapitalize="none"
 			placeholder="jundev@mecar.com"
 			value={email}
-			onChangeText={setEmail}
-			className="rounded-md h-10 px-2 border border-indigo-200"
+			onChangeText={(e)=> setEmail(e)}
+			className="rounded-md h-10 px-2 border border-indigo-200 w-full"
+		/>
+		<TextInput 
+			placeholder='password'
+			clearButtonMode='always'
+			value={password}
+			secureTextEntry
+			onChangeText={(text) => setPassword(text)}
+			keyboardAppearance='default'
+			className="rounded-md h-10 px-2 border border-indigo-200 w-full"
 		/>
 	</View>
   )
